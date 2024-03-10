31c8cf26effdde5140cd96e08c32b0e8dacc1afc
Maksym Sutkovenko <msutkovenko@readdle.com>
2024-03-10 11:23:20 +0200

    extact code to classes

--- main.py
+++ main.py
@@ -1,58 +1,125 @@
 import math
 import tkinter as tk
 
-def create_oval_polygon(canvas, center_x, center_y, width, height, rotation_angle, num_lines=40):
-    # Calculate angle step for the number of segments
-    assert num_lines % 2 == 0, "Number of lines must be even"
 
-    steps = num_lines
-    step_angel = 2 * math.pi / steps
+class Eye:
 
-    rotation_radians = math.radians(-rotation_angle)
-  
-    # Create a list of points for the oval polygon
-    points = []
-    oval_points = []
-    for i in range(steps):
-        # Calculate the angle for this segment
-        angle = i * step_angel
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        center_x: int,
+        center_y: int,
+        rotation_angle: int,
+        num_lines
+    ):
+        self.width = width
+        self.height = height
+        self.center_x = center_x
+        self.center_y = center_y
+        self.rotation_angle = rotation_angle
+        assert num_lines % 2 == 0, "Number of lines must be even"
+        self.num_lines = num_lines
 
-        # Calculate the oval points in the oval space
-        x = width / 2 * math.cos(angle)
-        y = height / 2 * math.sin(angle)
+    def draw(self, canvas: tk.Canvas):
+        self._create_oval_polygon(
+            canvas,
+            self.center_x,
+            self.center_y,
+            self.width,
+            self.height,
+            self.rotation_angle,
+            self.num_lines
+        )
 
-        # Rotate the oval points
-        rotated_x = x * math.cos(rotation_radians) - y * math.sin(rotation_radians)
-        rotated_y = x * math.sin(rotation_radians) + y * math.cos(rotation_radians)
+    def _create_oval_polygon(self, canvas, center_x, center_y, width, height, rotation_angle, num_lines):
+        # Calculate angle step for the number of segments
+        assert num_lines % 2 == 0, "Number of lines must be even"
 
-        points.extend([rotated_x, rotated_y])
-        oval_points.extend([center_x + rotated_x, center_y + rotated_y])
+        steps = num_lines
+        step_angel = 2 * math.pi / steps
 
-    canvas.create_polygon(oval_points, outline='blue', fill='black')
+        rotation_radians = math.radians(-rotation_angle)
+    
+        # Create a list of points for the oval polygon
+        points = []
+        oval_points = []
+        for i in range(steps):
+            # Calculate the angle for this segment
+            angle = i * step_angel
 
-    y_coords = points[1::2]
+            # Calculate the oval points in the oval space
+            x = width / 2 * math.cos(angle)
+            y = height / 2 * math.sin(angle)
 
-    theta = math.radians(rotation_angle)
-    a = width / 2
-    b = height / 2
-    for y in sorted(y_coords):
-        bd = y * math.sin(2 * theta) * (1/b**2 - 1/a**2)
-        ad = math.cos(theta)**2/a**2 + math.sin(theta)**2/b**2
-        c = y**2 * (math.sin(theta)**2/a**2 + math.cos(theta)**2/b**2) - 1
-        disc = bd**2 - 4 * ad * c
-        print(disc)
-        if disc < 0:
-            continue
+            # Rotate the oval points
+            rotated_x = x * math.cos(rotation_radians) - y * math.sin(rotation_radians)
+            rotated_y = x * math.sin(rotation_radians) + y * math.cos(rotation_radians)
 
-        x1 = (-bd - math.sqrt(disc)) / (2 * ad)
-        x2 = (-bd + math.sqrt(disc)) / (2 * ad)
-        print(f"x1: {x1}, x2: {x2}")
+            points.extend([rotated_x, rotated_y])
+            oval_points.extend([center_x + rotated_x, center_y + rotated_y])
 
-        x1 = center_x + x1
-        x2 = center_x + x2
-        y = center_y + y
+        canvas.create_polygon(oval_points, outline='blue', fill='black')
 
-        canvas.create_line(x1, y, x2, y, fill='red')
+        y_coords = points[1::2]
+
+        theta = math.radians(rotation_angle)
+        a = width / 2
+        b = height / 2
+        for y in sorted(y_coords):
+            bd = y * math.sin(2 * theta) * (1/b**2 - 1/a**2)
+            ad = math.cos(theta)**2/a**2 + math.sin(theta)**2/b**2
+            c = y**2 * (math.sin(theta)**2/a**2 + math.cos(theta)**2/b**2) - 1
+            disc = bd**2 - 4 * ad * c
+            print(disc)
+            if disc < 0:
+                continue
+
+            x1 = (-bd - math.sqrt(disc)) / (2 * ad)
+            x2 = (-bd + math.sqrt(disc)) / (2 * ad)
+            print(f"x1: {x1}, x2: {x2}")
+
+            x1 = center_x + x1
+            x2 = center_x + x2
+            y = center_y + y
+
+            canvas.create_line(x1, y, x2, y, fill='red')
+
+
+class Eyes:
+
+    def __init__(
+        self,
+        spacing: int,
+        rotation_angle: int,
+        eye_width: int,
+        eye_height: int,
+        eye_num_lines: int
+    ):
+        eye_center_y = 172
+        eye1_center_x = 194
+        self.eye1 = Eye(
+            width=eye_width,
+            height=eye_height,
+            center_x=eye1_center_x,
+            center_y=eye_center_y,
+            rotation_angle=rotation_angle,
+            num_lines=eye_num_lines
+        )
+
+        eye2_center_x = eye1_center_x + eye_width / 2 + spacing + eye_width / 2
+        self.eye2 = Eye(
+            width=eye_width,
+            height=eye_height,
+            center_x=eye2_center_x,
+            center_y=eye_center_y,
+            rotation_angle=180 - rotation_angle,
+            num_lines=eye_num_lines
+        )
+
+    def draw(self, canvas: tk.Canvas):
+        self.eye1.draw(canvas)
+        self.eye2.draw(canvas)
     
 
 if __name__ == '__main__':
@@ -64,17 +131,13 @@ if __name__ == '__main__':
     canvas = tk.Canvas(root, width=680, height=340, bg='black')
     canvas.pack()
 
-    eye_width = 188
-    eye_height = 124
-    eyes_spacing = 92
-    eye_center_y = 172
-
-    eye1_center_x = 194
-    eye1_rotation_angle = -16
-    create_oval_polygon(canvas, eye1_center_x, eye_center_y, eye_width, eye_height, eye1_rotation_angle)
-
-    eye2_center_x = eye1_center_x + eye_width / 2 + eyes_spacing + eye_width / 2
-    eye2_rotation_angle = 180 - eye1_rotation_angle
-    create_oval_polygon(canvas, eye2_center_x, eye_center_y, eye_width, eye_height, eye2_rotation_angle)
-  
+    eyes = Eyes(
+        spacing=92,
+        rotation_angle=-16,
+        eye_width=188,
+        eye_height=124,
+        eye_num_lines=100
+    )
+    eyes.draw(canvas)
+    
     root.mainloop()
