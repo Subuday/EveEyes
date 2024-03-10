f75f3110601f8c72041448d51a8e2915a00855d7
Maksym Sutkovenko <msutkovenko@readdle.com>
2024-03-10 11:47:39 +0200

    separate layouting and drawing

--- main.py
+++ main.py
@@ -13,29 +13,10 @@ class Eye:
         rotation_angle: int,
         num_lines
     ):
-        self.width = width
-        self.height = height
-        self.center_x = center_x
-        self.center_y = center_y
-        self.rotation_angle = rotation_angle
         assert num_lines % 2 == 0, "Number of lines must be even"
-        self.num_lines = num_lines
+        self.lines = self._create_lines(width, height, center_x, center_y, rotation_angle, num_lines)
 
-    def draw(self, canvas: tk.Canvas):
-        self._create_oval_polygon(
-            canvas,
-            self.center_x,
-            self.center_y,
-            self.width,
-            self.height,
-            self.rotation_angle,
-            self.num_lines
-        )
-
-    def _create_oval_polygon(self, canvas, center_x, center_y, width, height, rotation_angle, num_lines):
-        # Calculate angle step for the number of segments
-        assert num_lines % 2 == 0, "Number of lines must be even"
-
+    def _create_lines(self, width, height, center_x, center_y, rotation_angle, num_lines):
         steps = num_lines
         step_angel = 2 * math.pi / steps
 
@@ -43,7 +24,6 @@ class Eye:
     
         # Create a list of points for the oval polygon
         points = []
-        oval_points = []
         for i in range(steps):
             # Calculate the angle for this segment
             angle = i * step_angel
@@ -57,33 +37,35 @@ class Eye:
             rotated_y = x * math.sin(rotation_radians) + y * math.cos(rotation_radians)
 
             points.extend([rotated_x, rotated_y])
-            oval_points.extend([center_x + rotated_x, center_y + rotated_y])
-
-        canvas.create_polygon(oval_points, outline='blue', fill='black')
 
         y_coords = points[1::2]
 
-        theta = math.radians(rotation_angle)
         a = width / 2
         b = height / 2
+        theta = math.radians(rotation_angle)
+        lines = []
         for y in sorted(y_coords):
             bd = y * math.sin(2 * theta) * (1/b**2 - 1/a**2)
             ad = math.cos(theta)**2/a**2 + math.sin(theta)**2/b**2
             c = y**2 * (math.sin(theta)**2/a**2 + math.cos(theta)**2/b**2) - 1
             disc = bd**2 - 4 * ad * c
-            print(disc)
             if disc < 0:
                 continue
 
             x1 = (-bd - math.sqrt(disc)) / (2 * ad)
             x2 = (-bd + math.sqrt(disc)) / (2 * ad)
-            print(f"x1: {x1}, x2: {x2}")
 
             x1 = center_x + x1
             x2 = center_x + x2
             y = center_y + y
 
-            canvas.create_line(x1, y, x2, y, fill='red')
+            lines.append((x1, y, x2, y))
+        
+        return lines
+
+    def draw(self, canvas: tk.Canvas):
+        for x1, y1, x2, y2 in self.lines:
+            canvas.create_line(x1, y1, x2, y2, fill='red')
 
 
 class Eyes:
