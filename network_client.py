b8563a5495fc861a38a3829c750667e6013b2ffa
Subuday <mr.gigant977@gmail.com>
2024-03-30 09:51:49 +0200

    Add mp3 -> pcm converting

--- network_client.py
+++ network_client.py
@@ -1,18 +1,29 @@
 import asyncio
+from asyncio import subprocess, create_subprocess_exec
+import ffmpy
+from ffmpy import FFmpeg
 import websockets
+from multiprocessing import Queue
 
 class NetworkClient:
-    def __init__(self, input: asyncio.Queue, output: asyncio.Queue):
+    def __init__(self, input: Queue, output: Queue):
         self.uri = "wss://ws-dummy-production.up.railway.app"
         self.input = input
         self.output = output
         self.websocket = None
 
     async def _receive_data(self):
+        ff = FFmpeg(
+            inputs={'pipe:0': None},
+            outputs={'pipe:1': '-f s16le -acodec pcm_s16le -ar 44100 -ac 2'}
+        )
+
         while True:
             data = await self.websocket.recv()
-            await self.output.put(data)
-
+            print("Get some data from server!")
+            out, _ = ff.run(input_data=data, stdout=subprocess.PIPE)
+            print("Convert data to pcm!")
+            self.output.put(out)
 
     async def _send_data(self):
         pass
@@ -20,7 +31,12 @@ class NetworkClient:
     async def run(self):
         async with websockets.connect(self.uri) as websocket:
             self.websocket = websocket
-
             await asyncio.gather(
+                asyncio.create_task(self._send_data()),
                 asyncio.create_task(self._receive_data())
             )
+
+
+def network_client_main(input: Queue, output: Queue):
+    client = NetworkClient(input, output)
+    asyncio.run(client.run())
